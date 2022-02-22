cbde583bb2374b5b88f8e9acaebaf15530ae99bb
Anastasiia Kuznietsova <anastasia.kuznecova2110@gmail.com>
2022-02-22 20:18:46 +0200

    tenth HomeWork

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -92,28 +92,27 @@ window.addEventListener("DOMContentLoaded", () => {
     setClock(".timer", deadline);
     // Modal
 
-
     const modalTrigger = document.querySelectorAll("[data-modal]"), //Кнопки,вызывающее модальное окно
         modal = document.querySelector(".modal"), // само модальное окно
         modalCloseBtn = document.querySelector("[data-close]"); //отвечает за закрытие модального окна
 
-     function openModal() {
+    function openModal() {
         modal.classList.add("show");
         modal.classList.remove("hide");
         // Или вместо 2х строк - modal.classList.toggle("show");
         document.body.style.overflow = "hidden"; //добавляем стиль,который не дает прокручивать страницу
-        clearInterval(modalTimerId);
+        // clearInterval(modalTimerId);
     }
 
     modalTrigger.forEach(btn => {
-        btn.addEventListener("click",openModal);
+        btn.addEventListener("click", openModal);
     });
 
     function closeModal() {
         modal.classList.add("hide");
         modal.classList.remove("show");
         // Или вместо 2х строк - modal.classList.toggle("show");
-        document.body.style.overflow = " ";
+        document.body.style.overflow = "";
     }
 
     modalCloseBtn.addEventListener("click", closeModal);
@@ -133,15 +132,80 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     //откр мод окна через какое то опред время 
-    const modalTimerId = setTimeout(openModal, 3000);
+    // const modalTimerId = setTimeout(openModal, 3000);
 
     //когда кл долистал до конца страницу - откр мод окно  
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
             openModal();
-            window.removeEventListener("scroll", showModalByScroll);//удал откр мод  окна после первого долистывания до конца стр
+            window.removeEventListener("scroll", showModalByScroll); 
+            //удал откр мод  окна после первого долистывания до конца стр
         }
-
     }
     window.addEventListener("scroll", showModalByScroll);
+
+    //Используем классы для карточек 
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+   const infoCard1 = new MenuCard(
+        "img/tabs/vegy.jpg", 
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    );
+    infoCard1.render();
+    // или если больше не нужно будет далее использовать этот объект то - new MenyCard().render();
+
+    const infoCard2 = new MenuCard(
+        "img/tabs/elite.jpg", 
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        ".menu .container"
+    );
+    infoCard2.render();
+
+    const infoCard3 = new MenuCard(
+        "img/tabs/post.jpg", 
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        22,
+        ".menu .container"
+    );
+    infoCard3.render();
 });
